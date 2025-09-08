ad4bcb8e5830054d77405654f74da9fbc89924b0
Lykamopia <www.alex94lykam@gmail.com>
2025-09-08 07:00:25 +0000

    After creating a new requisition, automatically redirect the user to the

--- src/app/(app)/new-requisition/page.tsx
+++ src/app/(app)/new-requisition/page.tsx
@@ -1,6 +1,15 @@
 
+'use client';
+
 import { NeedsRecognitionForm } from '@/components/needs-recognition-form';
+import { useRouter } from 'next/navigation';
 
 export default function NewRequisitionPage() {
-    return <NeedsRecognitionForm />;
+    const router = useRouter();
+
+    const handleSuccess = () => {
+        router.push('/requisitions');
+    }
+
+    return <NeedsRecognitionForm onSuccess={handleSuccess} />;
 }
